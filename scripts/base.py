09c9a52c2d4601e67a56ddd55acbb66c717992ce
Jay-Y <570440569@qq.com>
2019-07-05 20:23:02 +0800

    模版完善

--- scripts/base.py
+++ scripts/base.py
@@ -5,10 +5,9 @@ import logging
 import os
 from logging.config import fileConfig
 
-
 class Base:
     def __init__(self):
         config_path = os.getcwd() + '/conf/logger.conf'
         fileConfig(config_path)
-        self.logger = logging.getLogger()
+        self.logger = logging.getLogger(__name__)
         self.const_config = None
